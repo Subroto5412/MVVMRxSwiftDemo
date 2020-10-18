6cf937a19eba5f04beb2eb8d682228c8c821c3f1
Subroto Mohonto <subroto.mohonto@gmail.com>
2020-10-18 19:14:32 +0600

    Initial Commit

---
+++ MVVMRxSwiftDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMRxSwiftDemo
+//
+//  Created by Subroto Mohonto on 18/10/20.
+//  Copyright © 2020 subroto training. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
